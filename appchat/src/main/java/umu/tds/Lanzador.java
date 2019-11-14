56b37e8f4038baf5c48cefec0e5e2c95d7cf9dc3
areszao <joseantonio.pastorv@um.es>
2019-11-14 18:20:12 +0100

    refactor

--- appchat/src/main/java/umu/tds/Lanzador.java
+++ appchat/src/main/java/umu/tds/Lanzador.java
@@ -1,12 +1,19 @@
 package umu.tds;
 
+import java.awt.EventQueue;
 import gui.LoginView;
 
-public class Lanzador 
-{
-    public static void main( String[] args )
-    {
-    	LoginView login = new LoginView();
-    	login.mostrarVentana();
-    }
+public class Lanzador {
+	public static void main(final String[] args){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginView ventana = new LoginView();
+					ventana.mostrarVentana();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
